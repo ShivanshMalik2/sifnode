bcdf509170e7ad7ae2ce1d6ade7f9c177bcbabdd
Daniel Portugal <daniportugal@gmail.com>
2021-09-29 16:32:58 -0300

    +copy manifest

--- smart-contracts/scripts/upgradeContracts.js
+++ smart-contracts/scripts/upgradeContracts.js
@@ -1,7 +1,14 @@
 const hardhat = require('hardhat');
+const fs = require('fs-extra');
 
 const support = require('./helpers/forkingSupport');
 const print = require('../test/helpers/helpers').colorLog;
+
+// If there is no DEPLOYMENT_NAME env var, we'll use the mainnet deployment
+const DEPLOYMENT_NAME = process.env.DEPLOYMENT_NAME || 'sifchain-1';
+
+// If there is no FORKING_CHAIN_ID env var, we'll use the mainnet id
+const CHAIN_ID = process.env.FORKING_CHAIN_ID || 1;
 
 async function main() {
   print('highlight', `~~~ Running script upgradeContracts.js ~~~`);
@@ -9,8 +16,17 @@ async function main() {
   // Makes sure we're forking
   support.enforceForking();
 
+  // Fetches the manifest
+  copyManifest();
+
   // Creates an instance of BridgeBank from the deployed code
-  const { instance: bridgeBank } = await support.getDeployedContract('sifchain-1', 'BridgeBank', 1);
+  const { instance: bridgeBank } = await support.getDeployedContract(
+    DEPLOYMENT_NAME,
+    'BridgeBank',
+    CHAIN_ID
+  );
+
+  // Fetches and logs the operator
   const operator_bb = await bridgeBank.operator();
   print('cyan', `-> Operator: ${operator_bb}`);
 
@@ -25,9 +41,7 @@ async function main() {
   print('cyan', `-> Operator: ${operator_cb}`);
   */
 
-  // Finds out which account is the proxy admin
-  //TODO: figure out how to use the correct account here
-  //const admin = await support.impersonateAccount(operator_bb, 1000000000);
+  // Impersonates the admin account
   const admin = await support.impersonateAccount(
     support.PROXY_ADMIN_ADDRESS,
     1000000000
@@ -41,12 +55,29 @@ async function main() {
   //const newCosmosBridgeFactory = await hardhat.ethers.getContractFactory('CosmosBridge');
   //await hardhat.upgrades.upgradeProxy(cosmosBridge, newCosmosBridgeFactory.connect(admin));
 
+  // Cleans up temporary files
+  cleanup();
+
   print('greenHighlight', '~~~ DONE! ~~~');
+}
+
+// Copies the manifest to the right place (where Hardhat wants it)
+function copyManifest() {
+  print('cyan', `Fetching the correct manifest`);
+
+  fs.copySync(`./deployments/sifchain-1/.openzeppelin/mainnet.json`, `./.openzeppelin/mainnet.json`);
+}
+
+// Deletes temporary files (the copied manifest)
+function cleanup() {
+  print('cyan', `Cleaning up temporary files`);
+
+  fs.unlinkSync(`./.openzeppelin/mainnet.json`);
 }
 
 main()
   .catch((e) => {
+    cleanup();
     print('red', `${e}`);
-    console.error({ e });
   })
   .finally(() => process.exit(0));
